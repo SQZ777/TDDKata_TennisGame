c042d26d5ec47ba83dda3a56b9d527a8e205ce59
sqz777 <sqz777isme@gmail.com>
2018-01-15 22:17:19 +0800

    fail Love-All

--- TDDKata_TennisGame/UnitTest1.cs
+++ TDDKata_TennisGame/UnitTest1.cs
@@ -6,8 +6,19 @@ namespace TDDKata_TennisGame
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Love_All()
         {
+            var tennisGame = new TennisGame();
+            var score = tennisGame.Score();
+            Assert.AreEqual("Love-All",score);
+        }
+    }
+
+    public class TennisGame
+    {
+        public string Score()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
